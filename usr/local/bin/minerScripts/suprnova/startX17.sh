25abe2caca3491b862a1cb5135bf231ebe6cc8e2
Keino Uelze <keino@gmx.net>
2018-03-09 15:29:13 +0100

    added logrotate config and intensity parameter for x17 Alexis78 mining

--- usr/local/bin/minerScripts/suprnova/startX17.sh
+++ usr/local/bin/minerScripts/suprnova/startX17.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/alexis78ccminer -a x17 -o stratum+tcp://xvg-x17.suprnova.cc:7477 -u hans23.rig1 -p yeshe --api-bind=0.0.0.0:4068 --api-remote --max-temp=79
+/usr/local/bin/alexis78ccminer -a x17 -i 20 -o stratum+tcp://xvg-x17.suprnova.cc:7477 -u hans23.rig1 -p yeshe --api-bind=0.0.0.0:4068 --api-remote --max-temp=79
 
