af0f4d61a311b62e883a2ca151707fadf6ec47df
cue108 <keino@gmx.net>
2018-07-02 18:23:32 +0200

    Update startMultiInstance.sh
    improvements

--- usr/local/bin/minerScripts/suprnova/startMultiInstance.sh
+++ usr/local/bin/minerScripts/suprnova/startMultiInstance.sh
@@ -5,7 +5,7 @@ GPUS=(`nvidia-smi dmon -c 1 | grep -vE "^#" |  awk '{ print +$1 }'`)
 echo There are ${#GPUS[@]} GPUs available.
 
 # open persistance mode on all GPUs and setting all GPU power limit
-/usr/local/bin/scripts/prepareGpus.sh 230
+/usr/local/bin/scripts/prepareGpus.sh 190
 
 ProcessAndLogPrefix=x17VergeSuprnova
 
@@ -17,20 +17,25 @@ do
  if [ $? -eq 0 ]; then
 	connection="true"
 	for id in "${GPUS[@]}"; do
-		# we undergo MAX 10 attampts to start the mining process for the given GPU
+	 	# special GPU id related actions go here
+                if [ $id == 1 ] 
+                        then
+                        echo special driver action for GPU id $id
+                        #nvidia-smi -i $id -pl 150
+                fi
+                if [ $id == 4 ] 
+                        then
+                        echo special driver action for GPU id $id
+                        #nvidia-smi -i $id -pl 150
+                fi
+		# we undergo MAX 20 attampts to start the mining process for the given GPU
 		COUNTER=0
-		while [  $COUNTER -lt 10 ]; do
+		while [  $COUNTER -lt 20 ]; do
 			processOnGPU=(`nvidia-smi -q -i $id -d PIDS | grep  "Processes.*None"`)
-			#echo R = ${#test[@]}
-			if [ ${#processOnGPU[@]} -gt 0  ]
+                        processOnRAM=(`ps afx | grep -i screen | grep "\-d $id"`)
+                        if [ ${#processOnGPU[@]} -gt 0 ] && [ ${#processOnRAM[@]} -eq 0 ]
 				then
-				# special GPU id related actions go here
-				if [ $id == 5 ] 
-                                        then
-                                        echo special action for GPU id $id
-                                        nvidia-smi -i $id -pl 175
-                                        #continue
-                                fi
+				intensity=22
                                 # special GPU id related actions go here
                                 if [ $id == 6 ] 
                                         then
@@ -38,45 +43,23 @@ do
                                         nvidia-smi -i $id -pl 185
                                         #continue
                                 fi
-                                if [ $id == 7 ] 
-                                        then
-                                        echo special action for GPU id $id
-                                        nvidia-smi -i $id -pl 185
-                                        #continue
-                                fi
-                                if [ $id == 8 ] 
-                                        then
-                                        echo special action for GPU id $id
-                                        nvidia-smi -i $id -pl 185
-                                        #continue
-                                fi
-                                if [ $id == 9 ] 
-                                        then
-                                        echo special action for GPU id $id
-                                        nvidia-smi -i $id -pl 185
-                                        #continue
-                                fi
-                                if [ $id == 10 ] 
-                                        then
-                                        echo special action for GPU id $id
-                                        nvidia-smi -i $id -pl 170
-                                        #continue
-                                fi
 				# default GPU id related actions go here
+				echo "starting miner process for GPU $id!"
 				/usr/local/bin/scripts/screen.sh $ProcessAndLogPrefix-GPU-$id \
 				"/usr/local/bin/alexis78ccminer \
 				-a x17 \
 				-d $id \
+				-i $intensity \
 				-o stratum+tcp://xvg-x17.suprnova.cc:7477 \
 				-u hans23.GPU$id \
 				-p yeshe \
 				--max-temp=79"				
-				sleep 2
+				sleep 7
 			else
 				break
 			fi
 		 	let COUNTER=COUNTER+1 
-		 	echo Attempt $COUNTER of 10 to start mining on GPU $id
+		 	echo Attempt $COUNTER of 20 to start mining on GPU $id
 		done
 	done
 else
