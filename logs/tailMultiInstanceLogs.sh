f382a1bf5a1b184b58e7a0dfd8babc0aefdaf15d
cue <keino@gmx.net>
2018-12-23 14:44:31 +0100

    Multi tail log

---
+++ logs/tailMultiInstanceLogs.sh
@@ -0,0 +1,13 @@
+
+/usr/local/bin/scripts/multi-tail.sh \
+/logs/x17VergeSuprnova-GPU-0.log \
+/logs/x17VergeSuprnova-GPU-1.log \
+/logs/x17VergeSuprnova-GPU-2.log \
+/logs/x17VergeSuprnova-GPU-3.log \
+/logs/x17VergeSuprnova-GPU-4.log \
+/logs/x17VergeSuprnova-GPU-5.log \
+/logs/x17VergeSuprnova-GPU-6.log \
+/logs/x17VergeSuprnova-GPU-7.log \
+/logs/x17VergeSuprnova-GPU-8.log \
+/logs/x17VergeSuprnova-GPU-9.log \
+/logs/x17VergeSuprnova-GPU-10.log
